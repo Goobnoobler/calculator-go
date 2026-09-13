f846b097342ef17e25e76e2ca12cdf3768066939
goobnoobler <marcushood45@gmail.com>
2026-09-13 20:22:07 +1000

    Fixed shunting yard algorithm not accumulating with double digit or greater numbers. Added Evaluator to main calculator setup.

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	"unicode"
+	"math"
+	"strconv"
+	"strings"
 )
 
 // "log"
@@ -13,32 +15,108 @@ import (
 //
 
 func main() {
-	expression := []rune("3+4*2-1")
-	EvalExpression(expression)
-	output, err := EvalExpression(expression)
-	fmt.Printf("%s %v\n", string(output), err)
+	var input = "0-100"
+
+	output, err := Shunt(input)
+
+	output2, err2 := Eval(output)
+	if err == nil && err2 == nil {
+		fmt.Printf("%v \n", output)
+		fmt.Printf("%v \n", output2)
+	} else {
+		fmt.Printf("%v \n", err)
+		fmt.Printf("%v \n", err2)
+	}
+
 }
 
-func EvalExpression(expression []rune) ([]rune, error) {
-	var output []rune
-	var operators []rune
+func Shunt(expression string) ([]string, error) {
+	var output []string
+	var operators []byte
+	var token strings.Builder
+	var prec = map[byte]int{'+': 1, '-': 1, '*': 2, '/': 2}
 
 	for i := range expression {
-		if unicode.IsDigit(expression[i]) {
-			output = append(output, expression[i])
+		if (expression[i] >= '0' && expression[i] <= '9') || expression[i] == '.' {
+			token.WriteByte(expression[i])
 		} else if expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/' {
-			var prec = map[rune]int{'+': 1, '-': 1, '*': 2, '/': 2}
+			if token.Len() > 0 {
+				output = append(output, token.String())
+				token.Reset()
+			}
 			for len(operators) > 0 && prec[operators[len(operators)-1]] >= prec[expression[i]] {
-				output = append(output, operators[len(operators)-1])
+				output = append(output, string(operators[len(operators)-1]))
 				operators = operators[:len(operators)-1] // shrinking it by 1
 			}
 			operators = append(operators, expression[i])
 		} else {
-			return nil, errors.New("Calculator only accepts Digits and +-*/.")
+			fmt.Println("Calculator only accepts Digits and +-*/.")
 		}
 	}
+	if token.Len() > 0 {
+		output = append(output, token.String())
+		token.Reset()
+	}
+
 	for i := len(operators); i > 0; i-- {
-		output = append(output, operators[i-1])
+		output = append(output, string(operators[i-1]))
 	}
 	return output, nil
+}
+
+func Eval(r []string) (float64, error) {
+
+	var calc []float64
+	var err error
+
+	for i := range r {
+		ans, parseErr := strconv.ParseFloat(r[i], 64)
+
+		if parseErr != nil {
+			if len(calc) > 1 {
+				var num1 = calc[len(calc)-2]
+				var num2 = calc[len(calc)-1]
+				var op string = r[i]
+
+				switch op {
+				case "+":
+					calc = calc[:len(calc)-2]
+					calc = append(calc, num1+num2)
+
+				case "-":
+					calc = calc[:len(calc)-2]
+					calc = append(calc, num1-num2)
+
+				case "*":
+					calc = calc[:len(calc)-2]
+					calc = append(calc, num1*num2)
+
+				case "/":
+					calc = calc[:len(calc)-2]
+					check0 := num1 / num2
+					if math.IsInf(check0, 0) {
+						err = errors.New("Cannot Divide by Zero.")
+						return 0, err
+					}
+					calc = append(calc, num1/num2)
+				default:
+					err = errors.New("Unknown Character in Expression.")
+					return 0, err
+				}
+			} else {
+				err = errors.New("expression is too short.")
+				return 0, err
+			}
+
+		} else {
+			calc = append(calc, ans)
+		}
+	}
+	if len(calc) == 1 {
+		return calc[0], nil
+	} else {
+		err = errors.New("Answer returned 2 values.")
+		return 0, err
+	}
+
 }
